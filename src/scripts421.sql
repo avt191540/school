92086923fcfd3b6a308ec38952ed3f7499e787f3
Igor <avt191540@yandex.ru>
2022-03-17 20:55:48 +0300

    course4Lesson2 started

---
+++ src/scripts421.sql
@@ -0,0 +1,6 @@
+Alter TABLE student
+    ADD CONSTRAINT age_constraint CHECK ( age >= 16 );
+ALTER TABLE student ALTER COLUMN name SET NOT NULL;
+ALTER TABLE student ADD CONSTRAINT name_unique UNIQUE (name);
+ALTER TABLE faculty ADD CONSTRAINT color_name_unique UNIQUE (color, name);
+ALTER TABLE student ALTER COLUMN age SET DEFAULT 20;
